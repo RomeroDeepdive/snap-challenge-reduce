d5e2c970b9b94854ad0035bd4c939fefa73e4bda
John Romero <Jromero326@cnm.edu>
2020-05-27 09:56:47 -0600

    SNap challenge complete with fetch and reduce

---
+++ snap-challenge-fetch-reduce/index.js
@@ -0,0 +1,32 @@
+//function fetchIpsum () {
+	//const targetElement = document.getElementById('target')
+	//fetch('https://jsonplaceholder.typicode.com/todos')
+		//.then(reply => (reply.json()))
+		//.then(body => {
+			//let accumulator = '';
+//fetch('https://jsonplaceholder.typicode.com/todos')
+			//body.forEach(index => {
+				//accumulator = `
+          //  ${accumulator}
+            //<p>${index}</p>
+          //`
+			//})
+
+			//targetElement.innerHTML = accumulator
+		//})
+//}
+
+
+function fetchIpsum () {
+	fetch('https://jsonplaceholder.typicode.com/todos')
+		.then(rawResponse => rawResponse.json())
+		.then(response => {
+			const formattedResponse = response.reduce((accumulator, currentValue) => {
+				 return `${accumulator} <h3>${currentValue.title}</h3>`
+			}, "")
+console.log(formattedResponse)
+			document.getElementById("target").innerHTML = formattedResponse
+		})
+}
+
+fetchIpsum()
